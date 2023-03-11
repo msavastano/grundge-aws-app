a0d1d0c381e3082132ae4f75466aebf5129cd3d9
msavast1 <msavast1@its.jnj.com>
2023-03-11 16:51:18 -0500

    ert

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -41,7 +41,7 @@ export default function Index() {
                       to="/join"
                       className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-red-700 shadow-sm hover:bg-red-50 sm:px-8"
                     >
-                      Sign up
+                      Sign up YOU!!!!
                     </Link>
                     <Link
                       to="/login"
